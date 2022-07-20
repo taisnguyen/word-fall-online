2bbee50d7e855fc71b8210fd39cc1382181cd92f
Tai Nguyen <taisnguyen123@yahoo.com>
2022-07-20 16:26:44 -0400

    Beautified formatting

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,22 +2,19 @@ const mode = process.env.NODE_ENV || "development";
 const path = require("path");
 
 module.exports = {
-  mode: mode,
-
-  entry: "./src/index.ts",
-  module: {
-    rules: [
-      {
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  }
+    mode: mode,
+    entry: "./src/index.ts",
+    module: {
+        rules: [{
+            use: "ts-loader",
+            exclude: /node_modules/,
+        }, ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    }
 };
