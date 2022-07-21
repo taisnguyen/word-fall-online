6f779402a5ee6d0633cc4008c1e69b1256b29b2d
Liu Jiawei <88jliu88@gmail.com>
2022-07-21 17:55:27 -0400

    fix: correct path to stylesheet

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { WordBoard, SquareWordBoard } from "./client/models/wordBoards";
 import { randNum } from "./client/utility/random";
-import "./styles/index.scss";
+import "./client/styles/index.scss";
 
 const squareWordBoard: SquareWordBoard = new SquareWordBoard(4);
 
