c2aaa4d9761769c8ef194b55cb049f727af0581a
Tai Nguyen <taisnguyen123@yahoo.com>
2022-07-20 20:16:35 -0400

    Refactored: separated src files into client/backend folders

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { WordBoard, SquareWordBoard } from "./models/wordBoards";
-import { randNum } from "./utility/random";
+import { WordBoard, SquareWordBoard } from "./client/models/wordBoards";
+import { randNum } from "./client/utility/random";
 import "./styles/index.scss";
 
 const squareWordBoard: SquareWordBoard = new SquareWordBoard(4);
