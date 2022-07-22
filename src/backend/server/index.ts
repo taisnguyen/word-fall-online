47a70b330f791d048d1bf02ada26ac2728649ee2
Tai Nguyen <taisnguyen123@yahoo.com>
2022-07-22 19:09:06 -0400

    Formatted and changed import from default export to just export

--- src/backend/server/index.ts
+++ src/backend/server/index.ts
@@ -1,10 +1,9 @@
 import dotenv from "dotenv";
-
 dotenv.config();
 
 // Set up server
 import { app, server } from "./setupServer.js";
 
 // Set up routes
-import setupRoutes from "./routes/index.js";
+import { setupRoutes } from "./routes/index.js";
 setupRoutes(app);
