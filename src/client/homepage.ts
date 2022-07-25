2b2d13f6f4aa1b4c9e42923bd5f221d45d4043e0
Tai Nguyen <taisnguyen123@yahoo.com>
2022-07-25 16:05:49 -0400

    Added page load board animation

--- src/client/homepage.ts
+++ src/client/homepage.ts
@@ -1,7 +1,33 @@
-import { WordBoard, SquareWordBoard } from "./models/wordBoards";
 import { randNum } from "./utility/random";
 import "./styles/homepage.scss";
 
-const squareWordBoard: SquareWordBoard = new SquareWordBoard(4);
+(() => {
 
-squareWordBoard.displayGrid();
+    function menuBoardFallingAnimation() {
+        let fallTime: number = 2.6;
+        document.querySelectorAll(".letter-tile").forEach((letterTile: HTMLElement) => {
+            letterTile.style.transform = "translateY(-" + window.innerHeight*Math.max(window.devicePixelRatio, 1) + "px)";")";
+            letterTile.style.pointerEvents = "none";
+
+            setTimeout(() => { 
+                fallTime -= 0.24 * (fallTime/5);
+                letterTile.style.transition = fallTime + "s";
+                letterTile.style.transitionTimingFunction = "cubic-bezier(.55,0,.68,.2)";
+                letterTile.style.removeProperty("transform");
+                setTimeout(() => { 
+                    letterTile.style.transition = "0.05s";
+                    letterTile.style.pointerEvents = "initial";
+                }, fallTime * 1000);
+             }, 0);
+
+            letterTile.style.display = "flex";
+        });
+    }
+
+    
+    window.onload = () => {
+        menuBoardFallingAnimation();
+    }
+
+
+})();
