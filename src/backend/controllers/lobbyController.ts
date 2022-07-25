7fb327eaead414af2796fc8e6663011533881dc2
Tai Nguyen <taisnguyen123@yahoo.com>
2022-07-24 23:24:07 -0400

    Added getLobbyPage endpoint action

--- src/backend/controllers/lobbyController.ts
+++ src/backend/controllers/lobbyController.ts
@@ -1,6 +1,17 @@
-const getLobbyPage = (req, res) => {
-    // const roomCode = req.query["room-code"];
-    // res.render("playPage");
+import { Request, Response } from "express";
+import { LobbyService } from "../services/lobbyService.js";
+import { Lobby } from "../models/lobby.js";
+
+const getLobbyPage = (req: Request, res: Response) => {
+    const lobbyCode: string = req.query["code"] as string;
+    const lobby: Lobby = LobbyService.getLobby(lobbyCode);
+    
+    // Lobby does not exist
+    if (lobby === null) {
+        res.render("homepage", { "letters": ["jcra", "ioet", "tai-", "snen"] });
+    }
+
+    res.render("lobby");
 }
 
 export { getLobbyPage };
