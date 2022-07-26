575c242d7c1c6b928037250ac062c33a347845ff
Tai Nguyen <taisnguyen123@yahoo.com>
2022-07-26 17:39:05 -0400

    Added: accessor methods and made changed property accessor to private from public

--- src/backend/models/lobby.ts
+++ src/backend/models/lobby.ts
@@ -1,8 +1,23 @@
+import { Player } from "./player.js";
+
 class Lobby {
-    public lobbyCode: string;
+    private lobbyCode: string;
+    private players: Array<Player> = [];
 
     public constructor(lobbyCode: string) {
         this.lobbyCode = lobbyCode;
+    }
+
+    public getLobbyCode(): string {
+        return this.lobbyCode;
+    }
+
+    public addPlayer(player: Player): void {
+
+    }
+
+    public getPlayers(): Array<Player> {
+        return this.players;
     }
 }
 
