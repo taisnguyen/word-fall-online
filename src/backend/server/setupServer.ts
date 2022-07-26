5328acc2bcb81c175081d056de5c61cb4f86a3c0
Tai Nguyen <taisnguyen123@yahoo.com>
2022-07-26 17:42:35 -0400

    Explicitly created HTTP server to pass in socket.io server

--- src/backend/server/setupServer.ts
+++ src/backend/server/setupServer.ts
@@ -1,4 +1,5 @@
-import express, { Application, Request, Response, NextFunction } from "express";
+import express, { Application} from "express";
+import { createServer } from "http";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 
@@ -7,10 +8,13 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 // Set up server
 const port = process.env.port || 3000;
 const app: Application = express();
-const server = app.listen(port, () => console.log(`listening on port ${port}`));
+const server = createServer(app);
+
 app.use(express.static(path.join(__dirname, "../../../dist")));
 app.use(express.json());
 app.set("views", path.join(__dirname, "../../client/views"));
 app.set("view engine", "ejs");
+
+server.listen(port, () => console.log(`listening on port ${port}`));
 
 export { app, server };
