08b829e6efd52fdad8431a22fd62300cfc531fe9
Tai Nguyen <taisnguyen123@yahoo.com>
2022-07-26 17:41:51 -0400

    Initialize network manager

--- src/backend/server/index.ts
+++ src/backend/server/index.ts
@@ -1,9 +1,18 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import { LobbyService } from "../services/lobbyService.js";
+LobbyService.createLobby("12341");
+
+
 // Set up server
 import { app, server } from "./setupServer.js";
 
 // Set up routes
 import { setupRoutes } from "./routes/index.js";
 setupRoutes(app);
+
+// Initialize network manager
+import { NetworkManager } from "../managers/networkManager.js";
+NetworkManager.init(server);
+
